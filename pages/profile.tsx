d06b72c0d991d8fadebe30cea82c65310702f378
julie yudowitch <julieyudo@gmail.com>
2022-07-08 05:29:11 -0400

    edited filename

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,9 @@
 import Nav from "../components/Nav/Nav";
-import useFirebaseAuth from "../firebase/useAUth";
+import useFirebaseAuth from "../firebase/useFirebaseAuth";
 import styles from '../styles/profile.module.css';
 
 const Profile = () => {
+    
     const { user, loading, logOut } = useFirebaseAuth();
     if (loading) {
         return "Loading, please wait..";
@@ -12,7 +13,8 @@ const Profile = () => {
         <Nav />
             <div className={styles.profile}>
                 <p>Welcome</p>
-          <button onClick={logOut}>Logout</button>
+                <button onClick={logOut}>Logout</button>
+                
         </div>
       </div>
     );
